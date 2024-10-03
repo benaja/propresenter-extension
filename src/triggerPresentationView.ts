4ae734442d4c1a90c3ade82c03e0fbffdccc3ea3
Benaja Hunzinger <benhu00@outlook.com>
2024-10-03 12:31:54 +0200

    compile action

--- src/triggerPresentationView.ts
+++ src/triggerPresentationView.ts
@@ -4,27 +4,31 @@ import { fetchActivePresentation } from "./api/presentations.ts";
 export function startPresentationListener() {
   let lastPresentation: string | null = null;
   async function setPresentationViewByLibrary() {
-    const response = await fetchActivePresentation();
-    if (!response.presentation) {
-      lastPresentation = null;
-      return;
-    }
+    try {
+      const response = await fetchActivePresentation();
+      if (!response.presentation) {
+        lastPresentation = null;
+        return;
+      }
 
-    if (lastPresentation === response.presentation.presentation_path) {
-      return;
-    }
-    lastPresentation = response.presentation.presentation_path;
+      if (lastPresentation === response.presentation.presentation_path) {
+        return;
+      }
+      lastPresentation = response.presentation.presentation_path;
 
-    const presentationPath = response.presentation.presentation_path;
-    const library =
-      presentationPath.split("/")[presentationPath.split("/").length - 2];
+      const presentationPath = response.presentation.presentation_path;
+      const library =
+        presentationPath.split("/")[presentationPath.split("/").length - 2];
 
-    if (library === "Lieder") {
-      console.log("This is a song presentation");
-      triggerMacro("Lieder Ansicht");
-    } else {
-      console.log("This is not a song presentation");
-      triggerMacro("Normale Ansicht");
+      if (library === "Lieder") {
+        console.log("This is a song presentation");
+        triggerMacro("Lieder Ansicht");
+      } else {
+        console.log("This is not a song presentation");
+        triggerMacro("Normale Ansicht");
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 
