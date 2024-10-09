ea8821d4ae6ad75f11c91a9582bf0072658854c7
Benaja Hunzinger <benhu00@outlook.com>
2024-10-09 18:09:08 +0200

    show correct error messages

--- src/api/electron/actions.ts
+++ src/api/electron/actions.ts
@@ -1,5 +1,5 @@
 import { ipcMain, ipcRenderer } from "electron";
-import { client, useChurchToolsClient } from "../churchtools";
+import { useChurchToolsClient } from "../churchtools";
 import { store, type StoreType } from "../../store";
 import { fetchLibraryElements, Song } from "../propresenter-official/library";
 import { fetchPresentation } from "../propresenter-official/presentations";
@@ -8,15 +8,35 @@ import {
   requestFolderAccess,
 } from "../propresenter-unofficial";
 
+const client = useChurchToolsClient();
+
+function parseErrorMessage(e) {
+  if (e.response.data) {
+    if (e.response.data.errors) {
+      return (
+        e.response.data.message +
+        ": " +
+        e.response.data.errors.map((error) => error.message).join("\n")
+      );
+    }
+
+    return `${e}`;
+  }
+}
+
 async function createSongIfNotExist(song: Song) {
   const presentation = await fetchPresentation(song.uuid);
 
   const file = await readPresentationFile(presentation.presentation_path);
 
-  const client = useChurchToolsClient();
-  let searchSongs = (await client.get("/songs", {
-    name: file.name,
-  })) as { id: number; name: string; ccli: string }[];
+  let searchSongs: { id: number; name: string; ccli: string }[] = [];
+  try {
+    searchSongs = (await client.get("/songs", {
+      name: file.name,
+    })) as { id: number; name: string; ccli: string }[];
+  } catch (e) {
+    throw new Error(parseErrorMessage(e));
+  }
 
   // Because the api cant search for ccli numbers, we filter the results manually
   // This is because sometimes two songs can have the same name, but different ccli numbers
@@ -31,16 +51,20 @@ async function createSongIfNotExist(song: Song) {
     return;
   }
 
-  const result = await client.oldApi("churchservice/ajax", "addNewSong", {
-    bezeichnung: file.name,
-    ccli: file.ccli ? file.ccli.songNumber : "",
-    author: file.ccli ? file.ccli.author : "",
-    songcategory_id: "0",
-    copyright: "",
-    tonality: "",
-    bpm: "",
-    beat: "",
-  });
+  try {
+    const result = await client.oldApi("churchservice/ajax", "addNewSong", {
+      bezeichnung: file.name,
+      ccli: file.ccli ? file.ccli.songNumber : "",
+      author: file.ccli ? file.ccli.author : "",
+      songcategory_id: "0",
+      copyright: "",
+      tonality: "",
+      bpm: "",
+      beat: "",
+    });
+  } catch (e) {
+    throw new Error(parseErrorMessage(e));
+  }
 }
 
 export const actions = {
